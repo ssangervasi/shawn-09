d2f9708ddbf094040f7d7c21be44d0f9e4f1d84a
James Harvey <james.harvey@samsara.com>
2020-03-22 14:52:51 -0700

    lint only

--- src/shawn.ts
+++ src/shawn.ts
@@ -1,50 +1,50 @@
 const rephrase = (phrase: string, pedantic: boolean = false): string => {
-	const shawnifier = Shawnifier.getInstance();
-	return shawnifier.execute(phrase, pedantic);
+	const shawnifier = Shawnifier.getInstance()
+	return shawnifier.execute(phrase, pedantic)
 }
 
 export class Shawnifier {
 	private static instance: Shawnifier;
 	private phraseLibrary: string[] = [];
 
-	private constructor() {};
+	private constructor() {}
 
 	public static getInstance(): Shawnifier {
 		if (!Shawnifier.instance) {
-			Shawnifier.instance = new Shawnifier();
+			Shawnifier.instance = new Shawnifier()
 		}
 
-		return Shawnifier.instance;
+		return Shawnifier.instance
 	}
 
 	execute (phrase: string, pedantic: boolean) {
-		if (this.phraseLibrary.includes(phrase)) return "Never send me that atrocious sentence again.";
-		this.phraseLibrary.push(phrase);
+		if (this.phraseLibrary.includes(phrase)) return 'Never send me that atrocious sentence again.'
+		this.phraseLibrary.push(phrase)
 
-		const words = phrase.split(' ');
-		const shawnificationFunc = pedantic ? this.pedanticallyShawnifyWord : this.shawnifyWord;
+		const words = phrase.split(' ')
+		const shawnificationFunc = pedantic ? this.pedanticallyShawnifyWord : this.shawnifyWord
 
-		return words.map(shawnificationFunc).join(' ');
+		return words.map(shawnificationFunc).join(' ')
 	}
 
 	pedanticallyShawnifyWord(word: string): string {
 		if (word.match(/[A-Z]/)) {
-			return 'Shaw';
+			return 'Shaw'
 		} else {
-			return 'shaw';
+			return 'shaw'
 		}
 	}
 
 	shawnifyWord(word: string): string {
-		if (word === 'Sean') return 'Shawn';
+		if (word === 'Sean') return 'Shawn'
 
-		let shawned = word;
-		shawned = shawned.replace(/SH/, 'Sh');
-		shawned = shawned.replace(/s(?![whp])/gi, '$&h');
-		shawned = shawned.replace(/c(?=[iy])/, 'sh');
-		shawned = shawned.replace(/(sh)(?:sh)+/gi, '$1');
-		shawned = shawned.replace(/(ss)(?:ss)+/gi, '$1');
-		return shawned;
+		let shawned = word
+		shawned = shawned.replace(/SH/, 'Sh')
+		shawned = shawned.replace(/s(?![whp])/gi, '$&h')
+		shawned = shawned.replace(/c(?=[iy])/, 'sh')
+		shawned = shawned.replace(/(sh)(?:sh)+/gi, '$1')
+		shawned = shawned.replace(/(ss)(?:ss)+/gi, '$1')
+		return shawned
 	}
 }
 
